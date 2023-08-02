fd5fa981336c8958bf1848f03c6252448aaecd8b
saikarun99 <peddireddysaikarun@gmail.com>
2023-08-02 22:11:52 +0530

    autopush

--- 04-variables.sh
+++ 04-variables.sh
@@ -4,7 +4,14 @@
 a=45
 b=56
 c=xyz
+#There is no concept of data types in linux
+# everything in linux is considered to be a string
+
+#How can I print the variable?
+#using the special character "$" we can print the value of the variable
 
 echo $a
 echo $b
 echo $c
+
+
