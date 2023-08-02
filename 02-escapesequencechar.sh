897d9046573f47cfc43f890bd325ec49fe3de22b
saikarun99 <peddireddysaikarun@gmail.com>
2023-08-02 21:41:43 +0530

    autopush

--- 02-escapesequencechar.sh
+++ 02-escapesequencechar.sh
@@ -1,2 +1,7 @@
 # \n: this indicated new line
 echo -e "line1\nline2\nline3"
+# whenever you are using special characters always enclose them in double quotes 
+# -e enables the escape characters
+
+# \t: this gives a tab space
+echo -e "cloud\tdevops\tare\tbooming"
