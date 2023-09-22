af8f54b1e13c23ac07e043d616c3c0d5044bf657
saikarun99 <peddireddysaikarun@gmail.com>
2023-09-22 20:09:17 +0530

    updated the file

---
+++ 07-exitcodes.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#Each and every action in linux will have exit code
+#it ranges from (0-255)
+#0 represents last successfully completed action
+# 1-255 represents partial success or partial failure or failure
+echo $?
