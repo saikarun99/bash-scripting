aaaec024dc8019e8a9af573b65e6f497969d8fe6
saikarun99 <peddireddysaikarun@gmail.com>
2023-09-22 22:12:53 +0530

    updated the file

---
+++ 10 -commands.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#There are 4 types of commands are available
+#1. binary (/bin, /sbin)
+#2. Aliases
+#3. Functions Functions are nothing but a set of commands that can be written in a sequence and can be called n number of times as per requirement
+#4. shell or built in commands
+
+#HOw to declare a function
+
+# f() {
+#     echo hai
+# }
+
+#THis is how we call function
+# f
