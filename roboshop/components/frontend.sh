109868a6974ed9c5d05edf90ec44ac336c0f789a
saikarun99 <peddireddysaikarun@gmail.com>
2023-09-23 12:15:46 +0530

    updated the file

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
-#validate the user who is running the script
- USER_ID=$(id -u)
- COMPONENT=frontend
- LOGFILE="/tmp/${COMPONENT}.log"
 
- if [$USER_ID -ne 0] ; then
-        echo "script is expected to be executed by the root user or with a sudo privilege
-        exit 1
- fi
+echo "configuring frontend"
+yum install nginx -y
+# systemctl enable nginx
+# systemctl start nginx
+# curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+# cd /usr/share/nginx/html
+# rm -rf *
+# unzip /tmp/frontend.zip
+# mv frontend-main/* .
+# mv static/* .
+# rm -rf frontend-main README.md
+# mv localhost.conf /etc/nginx/default.d/roboshop.conf
+
+#validate the user who is running the script
  
