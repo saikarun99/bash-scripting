1c55f730b53d7f21e7b225afd2ae0d9a7e27c0f2
saikarun99 <peddireddysaikarun@gmail.com>
2023-09-23 10:23:10 +0530

    updated the file

--- 14-ifelse.sh
+++ 14-ifelse.sh
@@ -52,4 +52,16 @@ action=$1
 
 if [ "$action" == "start" ]; then
     echo "starting payment service"
+    exit 0
+
+elif [ "$action" == stop ]; then
+        echo "stoping payment service"
+        exit 1
+elif [ "$action" == restart]; then
+        echo "restarting payment service"
+        exit 2
+else 
+
+echo "valid option is start only"
+exit 3
 fi
