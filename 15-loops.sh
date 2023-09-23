23f93c2cf1e616120087e789de0337ac708c7fa4
saikarun99 <peddireddysaikarun@gmail.com>
2023-09-23 10:40:19 +0530

    updated the file

---
+++ 15-loops.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#loops are used to execute sth for certain no of times
+
+#There are 2 times of majaor loops
+
+#1. For loop(When you know how many times it has to be executed)
+#2. While loop(When you don't know how many times it has to be executed)
+
+#for loop syntax
+
+for var in 10 20 30 40 50 ; do
+
+    echo "value for the loop is $val"
+done
