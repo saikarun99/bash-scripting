6e900407a88a369dd41925af816fbab72e40cde4
saikarun99 <peddireddysaikarun@gmail.com>
2023-09-23 18:59:19 +0530

    updated the file

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -1,3 +1,83 @@
-#!/bin/bash
+#! /bin/bash
+#validating the user is root user or not
+component=catalogue
+LOGFILE=/tmp/${component}.log
+stat()
+{
+if [ $1 == 0 ]; then
+       echo "successful"
+else 
+       echo "failed"
+       exit 2
+fi
+}
+USER_ID=$(id -u)
+if [ $USER_ID -ne 0 ]; then
+echo "Script is expected to be run by root user or with a sudo privilege \n \t Ex: sudo wrapper.sh frontend"
+exit 1
+fi
+echo "configuring catalogue"
+echo "installing nodejs"
+curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -
+yum install nodejs -y &>> ${LOGFILE}
+stat $?
 
-echo "I am catalogue"
+id ${APPUSER}
+if [ $? -ne 0 ]; then
+    echo "create application user account"
+    useradd roboshop
+    stat $?
+fi
+echo "downloading the ${component}"
+curl -s -L -o /tmp/catalogue.zip "https://github.com/stans-robot-project/catalogue/archive/main.zip"
+stat $?
+
+echo "copying the ${component} to ${APPUSER} home directory:"
+
+cd /home/${APPUSER}
+
+rm -rf ${COMPONENT}
+unzip -o /tmp/${component}.zip &>> ${LOGFILE}
+mv ${component}-main ${component}
+chown -R ${APPUSER}:${APPUSER} /home/${APPUSER}/${component}/
+stat $?
+
+echo "generating the ${component} artifacts"
+cd /home/${APPUSER}/${component}
+npm install &>> ${LOGFILE}
+stat $?
+
+echo "starting nginx"
+ systemctl enable nginx &>> ${LOGFILE}
+ systemctl start nginx &>> ${LOGFILE}
+ stat $?
+
+echo "downloading the frontend component"
+ curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+ stat $?
+
+ echo "clean up of default front end"
+ cd /usr/share/nginx/html &>> ${LOGFILE}
+ rm -rf * &>> ${LOGFILE}
+
+ stat $?
+
+ echo "extracting frontend"
+ unzip /tmp/frontend.zip &>> ${LOGFILE}
+ stat $?
+
+ echo "sorting the frontend files"
+mv frontend-main/* .
+mv static/* .
+rm -rf frontend-main README.md
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
+stat $?
+
+echo "restarting frontend"
+
+systemctl daemon-reload
+systemctl restart nginx
+
+
+
+ 
